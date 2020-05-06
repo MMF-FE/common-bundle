d723e4da1507657ea96603b6a76a64cc1e070c53
vfasky <vfasky@gmail.com>
2020-05-06 15:36:08 +0800

    feat html2nodes to es5

--- packages/html2nodes/webpack.config.js
+++ packages/html2nodes/webpack.config.js
@@ -19,6 +19,21 @@ let baseConfig = {
     },
     resolve: {
         alias: {}
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
     }
 }
 
