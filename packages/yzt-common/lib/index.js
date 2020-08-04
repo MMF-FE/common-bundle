5d99fac2eca13ada1da2cf65f20bceddb68c4dcd
Allenice Mo <994298628@qq.com>
2020-08-04 19:17:09 +0800

    fix dayjs

--- packages/yzt-common/lib/index.js
+++ packages/yzt-common/lib/index.js
@@ -5,6 +5,12 @@ import UAParser from 'ua-parser-js'
 import * as Lodash from 'lodash'
 import * as Sentry from '@sentry/browser'
 
+// dayjs 插件
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+import duration from 'dayjs/plugin/duration'
+Dayjs.extend(duration)
+Dayjs.extend(customParseFormat)
+
 // sentry 不安建议用 CDN 的方式加载
 const sobc_ = Sentry
 
